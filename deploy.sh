33d001402ec18525d3bc228b2490aeae4d28399e
Faisal Saeed <faisalee@gmail.com>
2020-01-23 01:38:07 +0800

    Fixed issues

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ echo "Stopping Contianers..."
 docker-compose stop
 
 echo "Removing Contianers..."
-docker container rm govtech_server1_1
-docker container rm govtech_server2_1
-docker container rm govtech_server3_1
-docker container rm govtech_server4_1
-docker container rm govtech_maxscale1_1
-docker container rm govtech_maxscale2_1
+docker container rm mariadb-ha-setup_server1_1
+docker container rm mariadb-ha-setup_server2_1
+docker container rm mariadb-ha-setup_server3_1
+docker container rm mariadb-ha-setup_server4_1
+docker container rm mariadb-ha-setup_maxscale1_1
+docker container rm mariadb-ha-setup_maxscale2_1
 
 echo "Rebuilding Contianers..."
 docker-compose up -d
@@ -22,7 +22,7 @@ echo "log_bin = mariadb-bin" >> server4.cnf
 docker-compose restart
 sleep 5
 
-docker exec -it govtech_server1_1 sh /go.sh Primary-Node
-docker exec -it govtech_server2_1 sh /go.sh Secondary-Node-1
-docker exec -it govtech_server3_1 sh /go.sh Secondary-Node-2
-docker exec -it govtech_server4_1 sh /go.sh Secondary-Node-3
+docker exec -it mariadb-ha-setup_server1_1 sh /go.sh Primary-Node
+docker exec -it mariadb-ha-setup_server2_1 sh /go.sh Secondary-Node-1
+docker exec -it mariadb-ha-setup_server3_1 sh /go.sh Secondary-Node-2
+docker exec -it mariadb-ha-setup_server4_1 sh /go.sh Secondary-Node-3
